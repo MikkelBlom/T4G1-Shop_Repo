9bcb71554cab4a340c86f12bed7774394d9d4a5e
Mikkel Blom <mikkel@ryborgblom.dk>
2026-02-12 10:30:20 +0100

    Added a shared project for universal classes (I.e. Product)

--- Shop.Presentation/obj/Debug/net10.0/Shop.Presentation.AssemblyInfo.cs
+++ Shop.Presentation/obj/Debug/net10.0/Shop.Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b68de8f6dd80296539d741f923e4418adaaafa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb27c49cdc29b4f9f860f1feb3b11669e45148bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Shop.Shared/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Shop.Shared;
+
+public class Product
+{
+    private string productID { public get; private set; }
+    private string productName { public get; private set; }
+    private double productPrice { public get; private set; }
+
+    public Product(string productID, string productName, double productPrice)
+    {
+        this.productID = productID;
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+}
