eb27c49cdc29b4f9f860f1feb3b11669e45148bf
Mikkel Blom <mikkel@ryborgblom.dk>
2026-02-12 10:12:26 +0100

    Added shared project + Basic UI setup.

--- Shop.Domain/obj/Debug/net10.0/Shop.Domain.AssemblyInfo.cs
+++ Shop.Domain/obj/Debug/net10.0/Shop.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+500f81f6addc99baa0fbfb875264c5dc1cb5b8a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74b68de8f6dd80296539d741f923e4418adaaafa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Shop.Presentation/Views/FooterView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Shop.Presentation;
+
+public partial class FooterView : UserControl
+{
+    public FooterView()
+    {
+        InitializeComponent();
+    }
+}
