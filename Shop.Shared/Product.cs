3676da190a66aee2dd9354a9478c118efb27c716
Mikkel Blom <mikkel@ryborgblom.dk>
2026-03-03 09:40:14 +0100

    Added category to Product class.

--- Shop.Shared/Product.cs
+++ Shop.Shared/Product.cs
@@ -5,11 +5,13 @@ public class Product
     private string productID;
     private string productName;
     private double productPrice;
+    private string productCategory;
 
-    public Product(string productID, string productName, double productPrice)
+    public Product(string productID, string productName, double productPrice, string productCategory)
     {
         this.productID = productID;
         this.productName = productName;
         this.productPrice = productPrice;
+        this.productCategory = productCategory;
     }
 }
