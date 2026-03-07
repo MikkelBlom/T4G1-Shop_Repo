40f7bad395ae8ca1668fd5e41b8967d99daa0490
Christian Tovdal <christian_tovdal@hotmail.com>
2026-03-07 16:34:03 +0100

    Made PIP-Usercontrol with initial look

---
+++ Shop.Presentation/PIP-UserControl.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Shop.Presentation;
+
+public partial class PIP_UserControl : UserControl
+{
+    public PIP_UserControl()
+    {
+        InitializeComponent();
+    }
+}
