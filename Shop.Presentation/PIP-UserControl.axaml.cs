b7b10656d0f4eac24b95f86184d9c1b8f115fc34
Christian Tovdal Schmidt <christian_tovdal@hotmail.com>
2026-03-11 11:42:47 +0100

    Updated PIP using ProduktPageCards style of "add to basket" and photo

--- Shop.Presentation/PIP-UserControl.axaml.cs
+++ Shop.Presentation/PIP-UserControl.axaml.cs
@@ -4,8 +4,23 @@ namespace Shop.Presentation;
 
 public partial class PIP_UserControl : UserControl
 {
+    private string ID { get; set; }
+    public string Title { get; set; }
+    public string Price { get; set; }
+    
     public PIP_UserControl()
     {
+        ID = "";
+        Title = "";
+        Price = "";
         InitializeComponent();
+    }
+    public PIP_UserControl(string id, string title, double price) : this()
+    {
+        ID = id;
+        Title = title;
+        Price = price.ToString("F2") + " DKK";
+
+        DataContext = this;
     }
 }
